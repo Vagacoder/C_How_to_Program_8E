91a273d132551ffcef26f127b6266fa4f6d7b5a1
Vagacoder <qiruihu@gmail.com>
2018-10-06 12:17:24 -0700

    add files

--- Ch07_PointerTest/src/Ch07_PointerTest.c
+++ Ch07_PointerTest/src/Ch07_PointerTest.c
@@ -31,5 +31,22 @@ int main(int argc, char *argv[]) {
 				printf("the value of index %d is %p.\n", i, xptr[i]);
 			}
 		}
+	puts("");
+	puts("Testing array now......");
+	int a[] = {34, 47, 59,62};
+	int *aptr;
+	aptr = a;
+	printf("a memory address is %p\n", &a);
+	printf("aptr memory address is %p\n", aptr);
+	printf("a value is %p\n", a);
+	printf("a[0] value is %d\n", a[0]);
+	printf("aptr value is %d\n", aptr[0]);
+	printf("aptr value is %d\n", *aptr);
+	printf("aptr value is %d\n", *(aptr+1));
+	printf("++aptr memory address is %p\n", ++aptr);
+	printf("aptr value is %d\n", aptr[0]);
+	printf("aptr value is %d\n", *aptr);
+	printf("++*aptr value is %d\n", ++*aptr);
+	printf("*++aptr value is %d\n", *++aptr);
 	return 0;
 }
